7928cc6661b034dba53b271d419c8fb3000b9c72
illiadanko <illia.a.danko@gmail.com>
2023-07-03 09:14:34 +0200

    Reuse current directory on split / create a new tmux window

--- navigate.el
+++ navigate.el
@@ -24,50 +24,89 @@
   :prefix "navigate-"
   :group 'evil)
 
-; Without unsetting C-h this is useless
-(global-unset-key (kbd "C-h"))
+(defvar tmux-fallback-directory "~"
+  "Tmux path to be used as a last resort.")
 
-; This requires windmove commands
+(defvar tmux-prefix-key "C-@" ; C-SPC (see tmux.conf)
+  "Tmux tmux.conf prefix-key.")
+
+                                        ; Without unsetting C-h this is useless
+(global-unset-key (kbd "C-h"))
+(global-unset-key (kbd tmux-prefix-key))
+
+                                        ; This requires windmove commands
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
 (defun tmux-navigate (direction)
   (let
-    ((cmd (concat "windmove-" direction)))
-      (condition-case nil
-          (funcall (read cmd))
-        (error
-          (tmux-command direction)))))
+      ((cmd (concat "windmove-" direction)))
+    (condition-case nil
+        (funcall (read cmd))
+      (error
+       (tmux-command direction)))))
 
 (defun tmux-command (direction)
   (shell-command-to-string
-    (concat "tmux select-pane -"
-      (tmux-direction direction))))
+   (concat "tmux select-pane -"
+           (tmux-direction direction))))
 
 (defun tmux-direction (direction)
   (upcase
-    (substring direction 0 1)))
+   (substring direction 0 1)))
+
+(defun tmux-current-directory (mode)
+  (pcase mode
+    ('dired-mode (dired-current-directory))
+    (_ default-directory)))
+
+(defun tmux-pane-directory ()
+  (expand-file-name
+   (or (vc-root-dir)
+       (tmux-current-directory major-mode)
+       tmux-fallback-directory)))
 
 (define-key evil-normal-state-map
-            (kbd "C-h")
-            (lambda ()
-              (interactive)
-              (tmux-navigate "left")))
+  (kbd "C-h")
+  (lambda ()
+    (interactive)
+    (tmux-navigate "left")))
 (define-key evil-normal-state-map
-            (kbd "C-j")
-            (lambda ()
-              (interactive)
-              (tmux-navigate "down")))
+  (kbd "C-j")
+  (lambda ()
+    (interactive)
+    (tmux-navigate "down")))
 (define-key evil-normal-state-map
-            (kbd "C-k")
-            (lambda ()
-              (interactive)
-              (tmux-navigate "up")))
+  (kbd "C-k")
+  (lambda ()
+    (interactive)
+    (tmux-navigate "up")))
 (define-key evil-normal-state-map
-            (kbd "C-l")
-            (lambda ()
-              (interactive)
-              (tmux-navigate "right")))
+  (kbd "C-l")
+  (lambda ()
+    (interactive)
+    (tmux-navigate "right")))
+(define-key evil-normal-state-map
+  (kbd (concat tmux-prefix-key " c"))
+  (lambda ()
+    (interactive)
+    (shell-command-to-string
+     (concat "tmux new-window -c "
+             (tmux-pane-directory)))))
+(define-key evil-normal-state-map
+  (kbd (concat tmux-prefix-key " %"))
+  (lambda ()
+    (interactive)
+    (shell-command-to-string
+     (concat "tmux split-window -h -c "
+             (tmux-pane-directory)))))
+(define-key evil-normal-state-map
+  (kbd (concat tmux-prefix-key " \""))
+  (lambda ()
+    (interactive)
+    (shell-command-to-string
+     (concat "tmux split-window -v -c "
+             (tmux-pane-directory)))))
 
 (provide 'navigate)
 
